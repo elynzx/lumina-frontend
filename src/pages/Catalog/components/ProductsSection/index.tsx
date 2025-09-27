3272f040ddaebe4afce56bdd2d08edd15fc826e1
Evelyn Pascual C <153583839+elynzx@users.noreply.github.com>
2025-09-27 15:27:48 -0500

    feat: add data to ProductsSection

--- src/pages/Catalog/components/ProductsSection/index.tsx
+++ src/pages/Catalog/components/ProductsSection/index.tsx
@@ -1,6 +1,15 @@
 import { FilterBar } from "../FilterBar";
 import { ProductCard } from "../ProductCard";
+import { data } from "@/constants/data";
 
+const getLocalImage = (localId: number) => {
+  const fotosLocal = data.fotosLocales.filter(_ => _.idLocal === localId);
+  return fotosLocal[0]?.urlFoto || '';
+}
+
+const getDistrict = (idDistrito: number) => {
+  return data.distritos.find(_ => _.idDistrito === idDistrito)?.nombreDistrito || '';
+}
 
 export const ProductsSection = () => {
   return (
@@ -8,16 +17,19 @@ export const ProductsSection = () => {
       <h2 className="text-title">Los mejores locales</h2>
       <FilterBar />
       <div className="section-grid">
-        <ProductCard imgUrl="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
-          title="Local en Miraflores"
-          district="Miraflores"
-          address="Av. Larco 123, Miraflores"
-          capacity={10}
-          pricePerHour={100}
-          buttonText="Reservar"
-          onClickAction={() => alert('Reservar')}
-        />
-
+        {data.locales.map((item) => (
+          <ProductCard
+            key={item.idLocal}
+            imgUrl={getLocalImage(item.idLocal)}
+            title={item.nombreLocal}
+            district={getDistrict(item.idDistrito)}
+            address={item.direccion}
+            capacity={item.aforoMaximo}
+            pricePerHour={item.precioHora}
+            buttonText="Reservar"
+            onClickAction={() => alert('Reservar')}
+          />
+        ))}
       </div>
     </div>
   )
--- src/pages/Catalog/index.tsx
+++ src/pages/Catalog/index.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/components/organism/Header";
 import { ProductsSection } from "./components/ProductsSection";
+import { Footer } from "@/components/organism/Footer";
 
 export const Catalog = () => {
     return (
         <>
             <Header onClickAction={() => { }} buttonText="Crear cuenta" isHome={false} />
             <ProductsSection />
+            <Footer />
         </>
     )
 };
