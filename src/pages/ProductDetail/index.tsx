acbb205e872c9a4a6a97e61a5d17895cf3c80387
Evelyn Pascual C <153583839+elynzx@users.noreply.github.com>
2025-09-29 20:30:49 -0500

    feat: add product photo-layout

--- src/pages/ProductDetail/index.tsx
+++ src/pages/ProductDetail/index.tsx
@@ -1,11 +1,14 @@
 import { SummaryForm } from "@/components/organism/SummaryForm";
+import { data } from "@/constants/data";
+import { PhotosLayout } from "./PhotosLayout";
 
 export const ProductDetail = () => {
     return (
-        <>
+        <div className="flex flex-col px-16 py-6  gap-6 justify-center items-center">
+            <PhotosLayout imgUrls={data.fotosLocales.filter(_ => _.idLocal === 1).map(_ => _.urlFoto)} description="Local para eventos" />
             <div className="w-full max-w-[440px] flex flex-col p-8">
                 <SummaryForm />
             </div>
-        </>
+        </div>
     )
 };
